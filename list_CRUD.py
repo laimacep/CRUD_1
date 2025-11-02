e4be0dba381e83d40b7a8776201f6d453fc60ac9
laimacep <laimacep88@gmail.com>
2025-11-02 13:49:04 +0200

    Added sort function, did some error testing and fixed them.

--- list_CRUD.py
+++ list_CRUD.py
@@ -3,18 +3,16 @@ import re
 from demo_data import *
 
 def view_options():
-    print(f'Choose what you want to do:\n 1. View...\n 2. Add... \n 3. Edit... \n 4. Delete... \n 5. Exit page')
+    print(f'Choose what you want to do:\n 1. View...\n 2. Add... \n 3. Edit... \n 4. Delete...\n 5. Sort...\n 6. Exit page')
 
 def selection():
-    option = (input(f'Select what you want to view: \n 1. Authors    2. Books    3. All\n').lower().strip(' .'))
+    option = (input(f'Select which data you want to work with: \n 1. Authors    2. Books\n').lower().strip(' .'))
     if option in ('1', '1.authors', 'authors'):
         return 'authors'
     if option in ('2', '2.books', 'books'):
         return 'books'
-    if option in ('3', '3.all', 'all'):
-        return 'all'
     else:
-        print('You chose an incorrect value')
+        print('Ooops, a typo. Try again!')
         return None
 
 def view_authors(authors):
@@ -23,46 +21,65 @@ def view_authors(authors):
 
 def view_books(books):
     for book in books:
-        print(f'{book['book_id']}. {book['title']} {book['genre']}')
+        print(f'{book['book_id']}. {book['title']} / {book['genre']}')
 
 def view_all(authors, books):
     for author in authors:
-        print(f'{author["name"]} {author["surname"]}')
-        author_books = [book for book in books if book['author_id'] == author["author_id"]]
+        print(f'{author['name']} {author['surname']}')
+        author_books = [book for book in books if book['author_id'] == author['author_id']]
         if author_books:
             for book in author_books:
-                print(f' - {book["title"]} {book["genre"]}')
+                print(f' - {book['title']} / {book['genre']}')
         else:
-            print(f'No books found for {author["name"]}')
+            print(f'No books found for {author['name']}')
 
 def add_author(id_counter,authors):
     print('You chose to add a new author')
     print('Write the name of the new author')
-    name = input()
+    name = input().title()
     print('Write the surname of the new author')
-    surname = input()
-    id_counter += 1
-    new_author = {'author_id': id_counter, 'name': name, 'surname': surname}
-    authors.append(new_author)
+    surname = input().title()
+    existing_a = None
+    for author in authors:
+        if author['name'].lower() == name.lower() and author['surname'].lower() == surname.lower():
+            existing_a = author
+            print('Seriously?! The author is already in the list...')
+    if not existing_a:
+        id_counter += 1
+        new_author = {'author_id': id_counter, 'name': name, 'surname': surname}
+        authors.append(new_author)
     return id_counter
 
-def add_book(book_id_counter,books):
+def add_book(book_id_counter,id_counter, authors, books):
     print('You chose to add a new book')
+    print('Please enter the author of the new book')
+    name = input(f'Enter the name of the author: ').title()
+    surname = input('Enter the surname of the author: ').title()
+    existing_author = None
+    for author in authors:
+        if author['name'].lower() == name.lower() and author['surname'].lower() == surname.lower():
+            existing_author = author
+            break
+    if not existing_author:
+        id_counter += 1
+        new_author = {'author_id': id_counter, 'name': name, 'surname': surname}
+        authors.append(new_author)
+        existing_author = new_author
     print('Write the tile of the new book')
-    title = input()
+    title = input().strip().title()
     print('Write the genre of the new book')
-    genre = input()
+    genre = input().strip().title()
     book_id_counter += 1
-    new_book = {'book_id': book_id_counter, 'title': title, 'genre': genre}
+    new_book = {'book_id': book_id_counter,'author_id':existing_author['author_id'], 'title': title, 'genre': genre}
     books.append(new_book)
-    return book_id_counter
+    return book_id_counter, id_counter
 
 def edit_author(authors):
     print('You chose to edit an author')
     print('Select which author you want to edit')
     view_authors(authors)
     edit_author_id = input()
-    allowed = r"^[\d]+$"
+    allowed = r'^[\d]+$'
     if re.match(allowed, edit_author_id):
         for i, author in enumerate(authors):
             if str(author['author_id']) == edit_author_id:
@@ -70,17 +87,17 @@ def edit_author(authors):
                 authors[i]['name'] = input()
                 print('Write new author surname')
                 authors[i]['surname'] = input()
-        else:
-            print('You chose and invalid id')
+            else:
+                print('Hmmm.. Type the ID of the author!')
     else:
-        print('You chose and invalid id')
+        print('Hmmm.. Type the ID of the author!')
 
 def edit_book(books):
     print('You chose to edit a book')
     print('Select which book you want to edit')
     view_books(books)
     edit_book_id = input()
-    allowed = r"^[\d]+$"
+    allowed = r'^[\d]+$'
     if re.match(allowed, edit_book_id):
         for i, book in enumerate(books):
             if str(book['book_id']) == edit_book_id:
@@ -88,55 +105,47 @@ def edit_book(books):
                 books[i]['title'] = input()
                 print('Write new book genre')
                 books[i]['genre'] = input()
-        else:
-            print('You chose and invalid id')
     else:
-        print('You chose and invalid id')
+        print('Hmmm.. Type the ID of the book!')
 
-def delete_author(authors):
+def delete_author(authors,books):
     print('You chose to delete an author')
     print('Select which author id you want to delete')
     view_authors(authors)
     del_author_id = input()
-    allowed = r"^[\d]+$"
+    allowed = r'^[\d]+$'
     if re.match(allowed, del_author_id):
-        for i, author in enumerate(authors):
-            if str(author['author_id']) == del_author_id:
-                print(f'Are you sure you want to delete this author?')
-                print('Type 0 to confirm delete and 1 to exit')
-                confirmation = input()
-                if confirmation == '0':
-                    for author in authors:
-                        if str(author['author_id']) == del_author_id:
-                            authors.remove(author)
-                            break
-                if confirmation == '1':
-                    pass
-        else:
+        del_author = next((author for author in authors if author['author_id'] == del_author_id),None)
+        if del_author is None:
             print('You chose and invalid id')
+        print(f'Are you sure you want to delete author {del_author['name']} {del_author['surname']}?')
+        print('Type YES to confirm delete and NO to cancel. REALLY THINK ABOUT IT!')
+        confirmation = input().lower()
+        if confirmation == 'yes':
+            authors[:] = [a for a in authors if a['author_id'] != del_author_id]
+            books[:] = [b for b in books if b['author_id'] != del_author_id]
+            print(f'Congrats, you deleted {del_author['name']} {del_author['surname']}. AND ALL HIS BOOKS! You dummy!')
+        elif confirmation == 'no':
+            print('Good. You made the right decision. Less work later on...')
     else:
-        print('You chose and invalid id')
+        print('Hmmm.. Type the ID of the author!')
 
 def delete_book(books):
     print('You chose to delete a book')
     print('Select which book you want to delete')
     view_books(books)
     del_book_id = input()
-    allowed = r"^[\d]+$"
+    allowed = r'^[\d]+$'
     if re.match(allowed, del_book_id):
-        for i, book in enumerate(books):
-            if str(book['book_id']) == del_book_id:
-                print(f'Are you sure you want to delete this book?')
-                print('Type 0 to confirm delete and 1 to exit')
-                confirmation = input()
-                if confirmation == '0':
-                    for book in books:
-                        if str(book['book_id']) == del_book_id:
-                            books.remove(book)
-                            break
-                if confirmation == '1':
-                    pass
-        else:
-            print('You chose and invalid id')
+        for book in books:
+            if book['book_id'] == del_book_id:
+                print(f'Are you sure you want to delete {book['title']}?')
+                print('Type YES to confirm delete and NO to cancel. REALLY THINK ABOUT IT!')
+                confirmation = input().lower()
+                if confirmation == 'yes':
+                    books[:] = [b for b in books if b['book_id'] != del_book_id]
+                    print(f'Congrats, you deleted {book['title']}. Now what?')
+                if confirmation == 'no':
+                    print('Good. You made the right decision. Less work later on...')
     else:
-        print('You chose and invalid id')
+        print('Hmmm.. Type the ID of the book!')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from list_CRUD import *
+import operator
 id_counter = 3
 book_id_counter = 6
 
@@ -7,19 +8,21 @@ while True:
     option = input()
     match option:
         case '1':
-            choice = selection()
-            if choice == 'authors':
+            option = (input(f'Select what you want to view: \n 1. Authors    2. Books    3. All\n').lower().strip(' .'))
+            if option in ('1', '1.authors', 'authors'):
                 view_authors(authors)
-            elif choice == 'books':
+            elif option in ('2', '2.books', 'books'):
                 view_books(books)
-            elif choice == 'all':
+            elif option in ('3', '3.all', 'all'):
                 view_all(authors, books)
+            else:
+                print('Ooops, a typo. Try again!')
         case '2':
             choice = selection()
             if choice == 'authors':
                 id_counter = add_author(id_counter, authors)
             elif choice == 'books':
-                book_id_counter = add_book(book_id_counter, books)
+                book_id_counter, id_counter = add_book(book_id_counter,id_counter, authors, books)
         case '3':
             choice = selection()
             if choice == 'authors':
@@ -29,11 +32,22 @@ while True:
         case '4':
             choice = selection()
             if choice == 'authors':
-                delete_author(authors)
+                delete_author(authors,books)
             elif choice == 'books':
                 delete_book(books)
         case '5':
-            print('You chose to exit the page')
+            choice = selection()
+            if choice == 'authors':
+                sorted_authors = sorted(authors, key=operator.itemgetter('name'))
+                for i in sorted_authors:
+                    print(f' {i['author_id']}. {i['name']} {i['surname']}')
+            elif choice == 'books':
+                sorted_books = sorted(books, key=operator.itemgetter('title'))
+                for i in sorted_books:
+                    print(f' {i['book_id']}. {i['title']}')
+        case '6':
+            print('Nothing to do, huh? Buh-bye then!')
             break
         case _:
-            print('You chose an invalid option')
+            print('Ooops, a typo. Try again!')
+
